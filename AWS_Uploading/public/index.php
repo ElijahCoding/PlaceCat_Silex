68d7cf1f0d721e62c5438199986731ec6dfb60b2
LittleElijah <cuiyeqing960904@gmail.com>
2018-04-14 23:18:50 +0300

    db configuration set up

--- AWS_Uploading/public/index.php
+++ AWS_Uploading/public/index.php
@@ -10,6 +10,17 @@ $app->register(new Silex\Provider\TwigServiceProvider, [
     'twig.path' => __DIR__ . '/../views'
 ]);
 
+$app->register(new Silex\Provider\DoctrineServiceProvider, [
+  'db.options' => [
+      'driver' => 'pdo_mysql',
+      'host' => 'localhost',
+      'dbname' => 'placecat',
+      'user' => 'root',
+      'password' => '',
+      'charset' => 'utf8',
+  ]
+]);
+
 $app->get('/', function () use ($app) {
   return $app['twig']->render('home.twig');
 });
