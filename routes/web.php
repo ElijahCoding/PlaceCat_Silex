4ecb4c6f160f71edfa2ae26bdc2265ff8675575b
LittleElijah <cuiyeqing960904@gmail.com>
2018-04-14 03:05:56 +0300

    grabbing random image from db

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Symfony\Component\HttpFoundation\{Request, Response};
 
-$app->get('/', function(Request $request, Silex\Application $app) {
-  return new Response('Home', 200);
-});
+$app->get('/{width}/{height}', function(Request $request, Silex\Application $app) {
+
+  $image = $app['db']->fetchAssoc("SELECT filename FROM images ORDER BY rand() LIMIT 1");
+
+
+
+  return new Response(null, 200);
+})->assert('width', '[0-9]+')->assert('height', '[0-9]+');
