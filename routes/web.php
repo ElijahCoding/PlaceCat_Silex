f29b5328bbb1f0349ca788afdfdbfecf13508449
LittleElijah <cuiyeqing960904@gmail.com>
2018-04-14 03:20:45 +0300

    rendering images

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,17 @@
 
 use Symfony\Component\HttpFoundation\{Request, Response};
 
-$app->get('/{width}/{height}', function(Request $request, Silex\Application $app) {
+$app->get('/{width}/{height}', function(Request $request, Silex\Application $app, $width, $height) {
 
   $image = $app['db']->fetchAssoc("SELECT filename FROM images ORDER BY rand() LIMIT 1");
 
+  $placeholder = $app['image']
+               ->make(__DIR__ . '/../public/img/' . $image['filename'])
+               ->fit($width, $height)
+               ->greyscale()
+               ->response('png');
 
-
-  return new Response(null, 200);
+  return new Response($placeholder, 200, [
+    'Content-Type' => 'image/png'
+  ]);
 })->assert('width', '[0-9]+')->assert('height', '[0-9]+');
